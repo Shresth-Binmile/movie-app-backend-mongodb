c0c233e474107810ee95cf05d9e5a8c97fa1e96d
Shresth-Binmile <shresth.gupta@binmile.com>
2024-08-07 15:16:35 +0530

    edited cors origin

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://movie-explorer-pi.vercel.app/#'],
+    origin: ['http://localhost:5173', 'https://movie-explorer-pi.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
